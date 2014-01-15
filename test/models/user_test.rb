067f86802a73076c48a98710021cf237582dd71c
Brett Chalupa <brettchalupa@gmail.com>
2014-01-15 09:27:07 -0500

    Write test for User.create_with_omniauth.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,4 +8,10 @@ class UserTest < ActiveSupport::TestCase
   test "should have many proposals" do
     assert_equal @brett.proposals.count, 2
   end
+
+  test "should create a user with omniauth" do
+    assert_difference('User.count') do
+      User.create_with_omniauth({ 'provider' => 'github', 'uid' => '12345', 'info' => { 'name' => 'Sebastian' }})
+    end
+  end
 end
