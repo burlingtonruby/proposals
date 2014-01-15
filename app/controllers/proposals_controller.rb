8b14a4ec5f81c5fc17ccc95a19500f949000cd41
Brett Chalupa <brettchalupa@gmail.com>
2014-01-14 20:43:11 -0500

    Expand upon Proposal controller and views.
    Add basic CRUD functionality.

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -7,11 +7,40 @@ class ProposalsController < ApplicationController
     @proposal = Proposal.new(proposal_params)
 
     if @proposal.save
-      flash[:success] = "Proposal successfully created. You will hear from us when the CFP closes. Thanks!"
       redirect_to @proposal
+      flash[:success] = "Proposal successfully created. You will hear from us when the CFP closes. Thanks!"
     else
       render 'new'
     end
+  end
+
+  def new
+    @proposal = Proposal.new
+  end
+
+  def show
+    @proposal = Proposal.find(params[:id])
+  end
+
+  def edit
+    @proposal = Proposal.find(params[:id])
+  end
+
+  def update
+    @proposal = Proposal.find(params[:id])
+
+    if @proposal.update_attributes(proposal_params)
+      redirect_to @proposal
+      flash[:success] = "Proposal successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Proposal.find(params[:id]).destroy
+    redirect_to proposals_path
+    flash[:notice] = 'Proposal destroyed.'
   end
 
   private
--- test/controllers/proposals_controller_test.rb
+++ test/controllers/proposals_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class ProposalsControllerTest < ActionController::TestCase
+  setup do
+    @rails_doesnt_scale = proposals(:rails_doesnt_scale)
+  end
+
   test 'should get index' do
     get :index
     assert_response :success
@@ -14,5 +18,38 @@ class ProposalsControllerTest < ActionController::TestCase
 
     assert_redirected_to proposal_path(assigns(:proposal))
     assert_equal 'Proposal successfully created. You will hear from us when the CFP closes. Thanks!', flash[:success]
+  end
+
+  test 'new should render correct layout' do
+    get :new
+    assert_template layout: 'layouts/application', partial: '_form'
+    assert_not_nil assigns(:proposal)
+  end
+
+  test 'should show proposal' do
+    get :show, id: @rails_doesnt_scale
+    assert_response :success
+    assert_not_nil assigns(:proposal)
+  end
+
+  test 'edit should render correct layout' do
+    get :edit, id: @rails_doesnt_scale
+    assert_template layout: 'layouts/application', partial: '_form'
+    assert_not_nil assigns(:proposal)
+  end
+
+  test 'should update proposal' do
+    patch :update, id: @rails_doesnt_scale.id, proposal: { title: "Rails Does Scale" }
+    assert_redirected_to proposal_path(assigns(:proposal))
+    assert_equal 'Proposal successfully updated.', flash[:success]
+  end
+
+  test 'should destroy proposal' do
+    assert_difference('Proposal.count', -1) do
+      delete :destroy, id: @rails_doesnt_scale.id
+    end
+
+    assert_redirected_to proposals_path
+    assert_equal 'Proposal destroyed.', flash[:notice]
   end
 end
