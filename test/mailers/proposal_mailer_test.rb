8a4096a9aed470c90035d40a70b0f474091afcde
beerlington <pete@lette.us>
2014-01-23 08:26:07 -0500

    Fixes broken email spec

--- test/mailers/proposal_mailer_test.rb
+++ test/mailers/proposal_mailer_test.rb
@@ -4,7 +4,7 @@ class ProposalMailerTest < ActionMailer::TestCase
   test "confirmation" do
     proposal = proposals(:rails_doesnt_scale)
     mail = ProposalMailer.confirmation(proposal.id)
-    assert_equal "Thanks for submitting a proposal to Burlington Ruby!", mail.subject
+    assert_equal "Thanks for submitting a proposal to the Burlington Ruby Conference!", mail.subject
     assert_equal ["brett@tacobell.com"], mail.to
     assert_equal ["info@burlingtonrubyconference.com"], mail.from
     assert_match "Hi", mail.body.encoded
