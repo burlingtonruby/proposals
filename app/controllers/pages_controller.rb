7661c079f8ba0372461f76b97657ba9a6b0c53fc
beerlington <pete@lette.us>
2014-01-26 09:45:08 -0500

    Allow applicants to view the welcome page
    This page contains info like dates and a summary of our selection
    process, so we should allow them to still get here by clicking on the
    conf name in the header.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,3 @@
 class PagesController < ApplicationController
   include UserHelper
-
-  def welcome
-    redirect_to proposals_path if current_user
-  end
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,18 +1,8 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
-  setup do
-    @brett = users(:brett)
-  end
-
   test "should get welcome" do
     get :welcome
     assert_response :success
-  end
-
-  test "welcome should redirect to proposals if signed in" do
-    sign_in @brett
-    get :welcome
-    assert_redirected_to proposals_path
   end
 end
